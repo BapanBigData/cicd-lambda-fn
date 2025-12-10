579384759e14ca058adb5de6c211340b2d3ae53b
BapanBigData <bapanmldl7892@gmail.com>
2025-12-10 08:20:19 +0530

    changes pushed

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,5 @@
 import json
 import requests
-import pandas as pd
 
 
 def lambda_handler(event, context):
@@ -8,10 +7,7 @@ def lambda_handler(event, context):
     print("Event Data -> ", event)
     response = requests.get("https://www.google.com/")
     print(response.text)
-
-    d = {"col1": [1, 2], "col2": [3, 4]}
-    df = pd.DataFrame(data=d)
-    print(df)
+    
     print("Demo Completed !!!")
 
     return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
